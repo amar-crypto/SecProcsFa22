6f564e66fec4eb1dc0c153ed1738d8b1d539dc68
amar-crypto <ramnaniamardeep1@gmail.com>
2022-10-20 00:06:13 -0700

    Part2 Updates

--- LAB1/Part2-DeadDrop/receiver.c
+++ LAB1/Part2-DeadDrop/receiver.c
@@ -2,43 +2,125 @@
 #include"util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
+#include <time.h>
 #define SAMPLES 10
+ 
+uint64_t get_cache_set_index(ADDR_PTR phys_addr)
+  {
+	    uint64_t mask = ((uint64_t) 1 << 15) - 1;
+	        return (phys_addr & mask) >> 6;
+  }
 
 int main(int argc, char **argv)
 {
-        uint64_t l2_latency[4096] = {0};
-	volatile char tmp;
-        uint64_t *prime_buffer = (uint64_t *)malloc(4096*8*sizeof(uint64_t));
-	// Put your covert channel setup code here
 
-       for (int j = 0; j < 4096; j++) {
-        tmp = prime_buffer[j*8];
+ int set_id[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+ uint64_t eviction_set[8][8];
+
+ volatile char tmp; 
+
+ int n = 8;
+ int miss_threshold = 70;
+
+ int o = 6;                          // log_2(64), where 64 is the line size
+ int s = 14;                         // log_2(16384), where 16384 is the number of cache sets
+ int two_o = 64;             // 64
+ int two_o_s = 512;   // 1048576
+ int b = 262144;
+
+ long int misses[8][8];
+ long int hits[8][8];
+
+ char *buffer = malloc((size_t) b);
+
+ uint64_t addr;
+
+ clock_t current_time, start_time;
+
+  for (int ind = 0; ind <8; ind++) {
+	  for (int way = 0; way <8; way++) {
+		  misses[ind][way] = 0;
+	  }
+  }
+
+  for (int ind = 0; ind <8; ind++) {
+	  for (int way = 0; way <8; way++) {
+		  hits[ind][way] = 0;
+	  }
+  }
+
+
+ for (int set_id_index = 0; set_id_index < 8; set_id_index++) {
+     int address_count = 0;
+     for (int set_index = 0; set_index < 512; set_index++) {
+       for (int way_index = 0; way_index < 8; way_index++) {
+	  addr = (uint64_t) (buffer + set_index * two_o_s + way_index * two_o);
+           if (get_cache_set_index(addr) == set_id[set_id_index]) {
+		 eviction_set[set_id_index][address_count] = addr;
+		 address_count++;
+	   }
+         }	 
        }
-       printf("receiver prime is done\n");
-	printf("Please press enter.\n");
+     }
 
-	char text_buf[2];
-	fgets(text_buf, sizeof(text_buf), stdin);
 
-	printf("Receiver now listening.\n");
+ for (int repeat=0; repeat < 100000; repeat++) {
+	for (int ind = 0; ind < 8; ind++) {
+          for (int way = 0; way < 8; way++){
+              uint64_t *temp_pointer = (uint64_t *) eviction_set[ind][way];
+	      tmp = *temp_pointer;
+	    }
+   	}
+    }
 
-	bool listening = true;
-	while (listening) {
-       for (int j = 0; j < 4096; j++) {
-       
-        l2_latency[j] = measure_one_block_access_time((uint64_t)&prime_buffer[j*8]);
-	if (l2_latency[j] > 200)
-	printf("L2 latency for index %d is %ld\n", j, l2_latency[j]);
-       
-   }
-		
+ start_time = clock();
+ current_time = start_time;
+
+while (current_time - start_time < 10000000) {
+current_time = clock();
+
+}	
+printf("Prime is done\n");       
+
+        bool listening = true;
+          for (int ind = 0; ind < 8; ind++) {
+	    for (int way = 0; way < 8; way++){
+         start_time = clock();
+         current_time = start_time;
+	    while (listening && current_time - start_time < 200) {
+            int time = measure_one_block_access_time(eviction_set[ind][way]);
+	    
+	   if (time < 700) {
+
+            if (time > miss_threshold)
+             //misses[ind][way]++;
+	     printf("For set %d we have miss\n", ind);		    
+	    else
+		    ;
+	     //hits[ind][way]++;
+           }		    
+  current_time = clock();      
+       }
+    }
 
 		// Put your covert channel code here
+}
 
-	}
-
-	printf("Receiver finished.\n");
-
+//  printf("Receiver finished.\n");
+//  for (int ind = 0; ind < 8; ind++) {
+//	  for (int way = 1; way <8; way++) {
+//		   misses[ind][0] += misses[ind][way];
+//	  }
+//		  printf("Misses are %ld for index %d is\n", misses[ind][0], ind);
+//  }
+//
+//  for (int ind = 0; ind <8; ind++) {
+//	  for (int way = 1; way <8; way++) {
+//		   hits[ind][0] += hits[ind][way];
+//	  }
+//            printf("Hits are %ld for index %d is\n", hits[ind][0], ind);
+//  }
 	return 0;
 }
 
